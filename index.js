ca2965034f03f08f721f7d322e59cb7dc1beb11c
chensonbh763 <41866678+chensonbh763@users.noreply.github.com>
2025-07-13 17:45:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-const webAppUrl = 'https://www.mfuture.com.br/lucremais/task.html';
+const webAppBaseUrl = 'https://www.mfuture.com.br/lucremais/task.html';
 
 bot.onText(/\/start/, (msg) => {
+  const userId = msg.from.id;
+  const fullUrl = `${webAppBaseUrl}?id=${userId}`;
+
   bot.sendMessage(msg.chat.id, '📋 Acesse suas tarefas abaixo:', {
     reply_markup: {
       inline_keyboard: [[
         {
           text: '📋 Abrir Tarefas',
-          web_app: { url: webAppUrl }
+          web_app: { url: fullUrl }
         }
       ]]
     }
